bddf70d8b65f8c7df4ed87c93e022ce5f1e37345
Nehal <patelnehal4u@gmail.com>
2014-08-22 14:37:14 +0530

    Removed api key and private key

--- app/Config/facebook.php
+++ app/Config/facebook.php
@@ -1,9 +1,9 @@
 <?php
 $config = array(
 		'Facebook' => array(
-				'appId'  => '402185779876907',
-                                'apiKey' => '402185779876907',
-				'secret' => 'c83e672bb01eae3e108d05e5ccfb87e3',
+				'appId'  => '*******************',
+                                'apiKey' => '*******************',
+				'secret' => '**************************************',
 				'cookie' => true,
 				'locale' => 'en_US',
 		)
